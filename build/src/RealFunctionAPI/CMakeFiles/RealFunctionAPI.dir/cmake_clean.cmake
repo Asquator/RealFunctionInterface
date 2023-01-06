afb77c9a2e47342bfddcd410b1f868dc6ddd64cf
asquator <vistalus@protonmail.com>
2023-01-06 21:13:13 +0200

    Restructured

---
+++ build/src/RealFunctionAPI/CMakeFiles/RealFunctionAPI.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/RealFunctionAPI.dir/binary_operation_function.cpp.o"
+  "CMakeFiles/RealFunctionAPI.dir/binary_operation_function.cpp.o.d"
+  "CMakeFiles/RealFunctionAPI.dir/polynomial.cpp.o"
+  "CMakeFiles/RealFunctionAPI.dir/polynomial.cpp.o.d"
+  "CMakeFiles/RealFunctionAPI.dir/real_function.cpp.o"
+  "CMakeFiles/RealFunctionAPI.dir/real_function.cpp.o.d"
+  "CMakeFiles/RealFunctionAPI.dir/sum_function.cpp.o"
+  "CMakeFiles/RealFunctionAPI.dir/sum_function.cpp.o.d"
+  "libRealFunctionAPI.a"
+  "libRealFunctionAPI.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/RealFunctionAPI.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
